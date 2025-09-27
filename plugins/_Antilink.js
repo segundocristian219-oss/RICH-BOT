3723e9c349ccef120d1412513918373cbb7626d7
AngelBotsD <angelperez22448@gmail.com>
2025-09-26 21:56:40 -0600

    Actualizar _Antilink.js

--- plugins/_Antilink.js
+++ plugins/_Antilink.js
@@ -1,1 +1,35 @@
-𝚡𝚍
+const urlRegex = /(https?:\/\/[^\s]+)/i
+
+export async function before(m, { conn, isAdmin }) {
+    if (m.isBaileys && m.fromMe) return !0
+    if (!m.isGroup) return !1
+
+    let chat = global.db.data.chats[m.chat]
+    if (!chat.antiLink) return !0
+
+    const body = (m.text || "").toLowerCase()
+    const isUrl = urlRegex.test(body)
+
+    if (isUrl && !isAdmin) {
+        try {
+            const thisGroupLink = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
+            if (body.includes(thisGroupLink.toLowerCase())) return !0
+            if (m.sender === this.user.jid) return !0
+
+            try {
+                await conn.sendMessage(m.chat, { delete: m.key })
+            } catch (e) {}
+
+            try {
+                await conn.reply(
+                    m.chat,
+                    `⚠️ *Se ha borrado un enlace enviado por *@${m.sender.split('@')[0]}*`,
+                    null,
+                    { mentions: [m.sender] }
+                )
+            } catch (e) {}
+        } catch (e) {}
+    }
+
+    return !0
+}
