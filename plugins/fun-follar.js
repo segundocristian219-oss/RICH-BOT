02e57e6eb87b30bf34c5a4ed8e06a7ca25ceb274
AngelBotsD <angelperez22448@gmail.com>
2025-09-29 19:47:57 -0600

    Actualizar fun-follar.js

--- plugins/fun-follar.js
+++ plugins/fun-follar.js
@@ -1,30 +1,21 @@
 let handler = async (m, { conn, command, text }) => {
-    // Si no hay texto y tampoco mensaje citado, pedimos que indique a quién
-    if (!text && !m.quoted) {
+    let user = m.mentionedJid?.[0] || m.quoted?.sender;
+
+    if (!user) {
         return m.reply(`*Ingresa el @ o responde al mensaje de la persona que quieras saber si te puedes ${command.replace('how', '')}*`);
     }
 
-    try {
-        // Detectar usuario: si menciona, usar la mención; si responde, usar el citado
-        let user = m.mentionedJid?.[0] || m.quoted?.sender || null;
-        let targetName = text || (await conn.getName(user));
+    let targetName = text || await conn.getName(user);
 
-        if (!user) return m.reply('*No pude encontrar al usuario.*');
-
-        m.reply(`🤤👅🥵 *𝐀𝐂𝐀𝐁𝐀𝐒 𝐃𝐄 𝐅𝐎𝐋𝐋𝐀𝐑𝐓𝐄𝐋@!*🥵👅🤤
+    m.reply(`🤤👅🥵 *𝐀𝐂𝐀𝐁𝐀𝐒 𝐃𝐄 𝐅𝐎𝐋𝐋𝐀𝐑𝐓𝐄𝐋@!*🥵👅🤤
 
 *𝙏𝙚 𝙖𝙘𝙖𝙗𝙖𝙨 𝙙𝙚 𝙛𝙤𝙡𝙡𝙖𝙧 𝙖 𝙡𝙖 𝙥𝙚𝙧𝙧𝙖 𝙙𝙚* *${targetName}* ⁩ *𝙖 𝟰 𝙥𝙖𝙩𝙖𝙨 𝙢𝙞𝙚𝙣𝙩𝙧𝙖𝙨 𝙩𝙚 𝙜𝙚𝙢𝙞𝙖 𝙘𝙤𝙢𝙤 𝙪𝙣𝙖 𝙢𝙖𝙡𝙙𝙞𝙩𝙖 𝙥𝙚𝙧𝙧𝙖 "𝐀𝐚𝐚𝐡.., 𝐀𝐚𝐚𝐡𝐡, 𝐬𝐢𝐠𝐮𝐞, 𝐧𝐨 𝐩𝐚𝐫𝐞𝐬, 𝐧𝐨 𝐩𝐚𝐫𝐞𝐬.." 𝙮 𝙡𝙖 𝙝𝙖𝙨 𝙙𝙚𝙟𝙖𝙙𝙤 𝙩𝙖𝙣 𝙧𝙚𝙫𝙚𝙣𝙩𝙖𝙙𝙖 𝙦𝙪𝙚 𝙣𝙤 𝙥𝙪𝙚𝙙𝙚 𝙨𝙤𝙨𝙩𝙚𝙣𝙚𝙧 𝙣𝙞 𝙨𝙪 𝙥𝙧𝙤𝙥𝙞𝙤 𝙘𝙪𝙚𝙧𝙥𝙤 𝙡𝙖 𝙢𝙖𝙡𝙙𝙞𝙩𝙖 𝙯𝙤𝙧𝙧𝙖!*
 
 *${targetName}* 
 🤤🥵 *¡𝐘𝐀 𝐓𝐄 𝐇𝐀𝐍 𝐅𝐎𝐋𝐋𝐀𝐃𝐎!* 🥵🤤`, null, { mentions: [user] });
-    } catch (err) {
-        console.log(err);
-    }
 }
 
 handler.help = ['follar'];
 handler.tags = ['fun'];
 handler.command = /^(Follar|violar)/i;
-
-handler.register = false;
 export default handler;
