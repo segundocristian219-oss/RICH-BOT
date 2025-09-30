35ef26988db77bec12eadb30d13d276547b3d694
AngelBotsD <angelperez22448@gmail.com>
2025-09-30 17:17:27 -0600

    Actualizar owner-salir.js

--- plugins/owner-salir.js
+++ plugins/owner-salir.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, command }) => {
   let id = text ? text : m.chat
-  let mensaje = '𝙲𝚘́𝚖𝚘 𝚝𝚞́ 𝙿𝚊𝚙𝚊́, 𝚃𝚎 𝚊𝚋𝚊𝚗𝚍𝚘𝚗𝚘 🏃🏻‍♂️ Whitxs Bot'
+  let mensaje = '𝙲𝚘́𝚖𝚘 𝚝𝚞́ 𝙿𝚊𝚙𝚊́, 𝚃𝚎 𝚊𝚋𝚊𝚗𝚍𝚘𝚗𝚘 🏃🏻‍♂️ baki Bot'
   await conn.sendMessage(m.chat, { text: mensaje, mentions: [m.sender] }, { quoted: m })
   await conn.groupLeave(id)
 }
