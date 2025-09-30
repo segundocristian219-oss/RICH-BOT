b80aeccb05c81430814a9abdd6bfea8832c34777
AngelBotsD <angelperez22448@gmail.com>
2025-09-29 20:18:21 -0600

    Actualizar fun-gay.js

--- plugins/fun-gay.js
+++ plugins/fun-gay.js
@@ -1,25 +1,23 @@
-
-let handler = async (m, { conn, groupMetadata}) => {
+let handler = async (m, { conn, groupMetadata }) => {
   let who = m.mentionedJid?.[0]
-? m.mentionedJid[0]
-: m.quoted
-? m.quoted.sender
-: m.sender;
+    ? m.mentionedJid[0]
+    : m.quoted
+    ? m.quoted.sender
+    : m.sender;
 
   let nro = Math.floor(Math.random() * 101); // Valor entre 0 y 100
   let mensaje = `@${who.split("@")[0]} es ${nro}% Gay 🏳️‍🌈.`;
 
-  await m.reply(mensaje, null, { mentions: [who]});
+  await m.reply(mensaje, null, { mentions: [who] });
 
-  // Envío del audio como archivo
+  // Envío del audio como audio normal
   await conn.sendFile(
     m.chat,
     'https://qu.ax/grQGD.m4a',
     'audio.mp3',
     null,
-    m,
-    true // Enviar como nota de voz
-);
+    m
+  );
 };
 
 handler.help = ['gay'];
