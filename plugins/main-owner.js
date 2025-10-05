f68573c88fa802dc9cead8c6ce3c1c4b7615c7bd
AngelBotsD <angelperez22448@gmail.com>
2025-10-04 19:46:58 -0600

    Actualizar main-owner.js

--- plugins/main-owner.js
+++ plugins/main-owner.js
@@ -1,4 +1,3 @@
-import fs from 'fs'
 import PhoneNumber from 'awesome-phonenumber'
 
 let handler = async (m, { conn }) => {
@@ -6,14 +5,12 @@ let handler = async (m, { conn }) => {
 
   m.react('⛩️')
 
-  const imageUrl = 'https://cdn.russellxz.click/46265152.jpeg'
   const numCreador = '5215561076182'
   const name = 'Cristian ⛩️'
   const empresa = 'Cristian - Servicios tecnológicos ⛩️'
   const correo = 'correo@empresa.com'
 
-  // Crear contenido de vCard
-  const vcardContent = `
+  const vcard = `
 BEGIN:VCARD
 VERSION:3.0
 N:;${name};;;
@@ -23,34 +20,16 @@ TEL;TYPE=CELL:${new PhoneNumber('+' + numCreador).getNumber('international')}
 EMAIL:${correo}
 END:VCARD`.trim()
 
-  // Guardar temporalmente en un archivo
-  const filePath = './owner.vcf'
-  fs.writeFileSync(filePath, vcardContent)
-
-  // Enviar la vCard como documento
   await conn.sendMessage(
     m.chat,
     {
-      document: { url: filePath },
-      fileName: 'Cristian.vcf',
-      mimetype: 'text/vcard',
-      contextInfo: {
-        mentionedJid: [m.sender],
-        externalAdReply: {
-          title: 'baki bot ⛩️',
-          body: 'baki bot ⛩️',
-          thumbnailUrl: imageUrl,
-          mediaType: 1,
-          showAdAttribution: true,
-          renderLargerThumbnail: true
-        }
+      contacts: {
+        displayName: name,
+        contacts: [{ vcard }]
       }
     },
     { quoted: m }
   )
-
-  // Opcional: borrar el archivo temporal después de enviarlo
-  fs.unlinkSync(filePath)
 }
 
 handler.help = ['owner']
