5d904fd38d85a6349bd7838fdfac551475265647
segundocristian219-oss <segundocristian219@gmail.com>
2025-10-14 00:31:02 -0600

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -392,7 +392,7 @@ está cerrada por órdenes del creador.`
 
 }[type]
 
-if (msg) return m.reply(msg + "\n\n> ✦ Hecho por Soyhernadez <3 ✦").then(_ => m.react('🏜️'))
+if (msg) return m.reply(msg + "\n\n> ✦ Hecho por SoyRich <3 ✦").then(_ => m.react('🏜️'))
 }
 
 let file = global.__filename(import.meta.url, true)
