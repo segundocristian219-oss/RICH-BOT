7209d2d82e275dea2dfb3074ca077ba7cf90c272
segundocristian219-oss <segundocristian219@gmail.com>
2025-11-28 19:51:33 -0600

    Actualizar owner-salir.js

--- plugins/owner-salir.js
+++ plugins/owner-salir.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, command }) => {
   let id = text ? text : m.chat
-  let mensaje = '𝙲𝚘́𝚖𝚘 𝚝𝚞́ 𝙿𝚊𝚙𝚊́, 𝚃𝚎 𝚊𝚋𝚊𝚗𝚍𝚘𝚗𝚘 🏃🏻‍♂️ baki Bot'
+  let mensaje = '𝙲𝚘́𝚖𝚘 𝚝𝚞́ 𝙿𝚊𝚙𝚊́, 𝚃𝚎 𝚊𝚋𝚊𝚗𝚍𝚘𝚗𝚘 🏃🏻‍♂️ @𝑺𝒙𝒏𝒕𝒐𝒔𝒔𝒔7'
   await conn.sendMessage(m.chat, { text: mensaje, mentions: [m.sender] }, { quoted: m })
   await conn.groupLeave(id)
 }
